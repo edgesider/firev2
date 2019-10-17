86a58bfa80715870bc4a4aef858e9cd523b1893b
ykai <yingkaidang@gmail.com>
2019-10-17 15:38:41 +0800

    update

--- completions/firev2_completion.fish
+++ completions/firev2_completion.fish
@@ -1,12 +1,12 @@
-complete -c v2 -x -a '(v2 list)' --condition '__fish_seen_subcommand_from start'
-complete -c v2 -x -a start --condition '__fish_v2_no_subcommand'
-complete -c v2 -x -a restart --condition '__fish_v2_no_subcommand'
-complete -c v2 -x -a stop --condition '__fish_v2_no_subcommand'
-complete -c v2 -x -a status --condition '__fish_v2_no_subcommand'
-complete -c v2 -x -a list --condition '__fish_v2_no_subcommand'
-complete -c v2 -x -a test --condition '__fish_v2_no_subcommand'
+complete -c firev2 -x -a '(firev2 list)' --condition '__fish_seen_subcommand_from start'
+complete -c firev2 -x -a start --condition '__fish_firev2_no_subcommand'
+complete -c firev2 -x -a restart --condition '__fish_firev2_no_subcommand'
+complete -c firev2 -x -a stop --condition '__fish_firev2_no_subcommand'
+complete -c firev2 -x -a status --condition '__fish_firev2_no_subcommand'
+complete -c firev2 -x -a list --condition '__fish_firev2_no_subcommand'
+complete -c firev2 -x -a test --condition '__fish_firev2_no_subcommand'
 
-function __fish_v2_no_subcommand
+function __fish_firev2_no_subcommand
     for i in (commandline -cop)
         if contains -- $i start status stop restart stat list ls test
             return 1
