ca17ca5c40a5547c5ecb2cb7b6d506390f6d8125
ykai <yingkaidang@gmail.com>
2019-10-17 16:37:27 +0800

    find configuration file ~/.config/firev2/firev2.conf instead of ~/.firev2.conf

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 import json
+import subprocess
 
 
 systemd_service = 'firev2.service'
@@ -7,13 +8,22 @@ link_target = None
 node_dir = None
 template_dir = None
 
+user = os.environ.get('SUDO_USER')
+if user is None:
+    home = os.environ['HOME']
+else:
+    # running as sudo
+    home = subprocess.Popen(
+            'getent passwd {} | cut -d: -f6'.format(user),
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            shell=True).communicate()[0].decode('utf8').strip()
+find_path = [
+        'firev2.conf',
+        os.path.join(home, '.config', 'firev2', 'firev2.conf'),
+        '/etc/firev2.conf',
+        ]
 
 def auto_load():
-    find_path = [
-            'firev2.conf',
-            os.path.join(os.environ['HOME'], '.firev2.conf'),
-            '/etc/firev2.conf',
-            ]
     for f in find_path:
         if os.path.isfile(f):
             load_from_file(f)
@@ -26,8 +36,12 @@ def load_from_file(filename):
     global node_dir
     global template_dir
 
-    with open(filename) as f:
+    path = os.path.abspath(filename)
+    config_dir = os.path.dirname(path)
+    config = os.path.basename(path)
+    os.chdir(config_dir)
+    with open(config) as f:
         config = json.load(f)
-    node_dir = config['node_dir']
-    template_dir = config['template_dir']
-    link_target = config['link_target']
+    node_dir = os.path.abspath(config['node_dir'])
+    template_dir = os.path.abspath(config['template_dir'])
+    link_target = os.path.abspath(config['link_target'])
